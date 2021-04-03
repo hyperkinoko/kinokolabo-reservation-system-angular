f2d7240c81efcad1539d893aab796af9894aabf3
kajimotoh <k.xdlalbx.p@gmail.com>
2021-04-03 11:15:02 +0900

    Write reservation class

---
+++ src/app/models/reservation.ts
@@ -0,0 +1,10 @@
+export class Reservation{
+  id: string;
+  studentId: string;
+  start: number;
+  end: number;
+  actualStart: number;
+  actualEnd: number;
+  price: number;
+  meetingUrl: string;
+}
