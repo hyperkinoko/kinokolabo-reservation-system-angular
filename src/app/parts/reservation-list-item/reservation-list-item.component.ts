2203f82e53dbb246bc249f60c5043e1bc08e5da9
kajimotoh <k.xdlalbx.p@gmail.com>
2021-04-03 13:59:34 +0900

    add momentjs

--- src/app/parts/reservation-list-item/reservation-list-item.component.ts
+++ src/app/parts/reservation-list-item/reservation-list-item.component.ts
@@ -1,4 +1,6 @@
+import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import {Reservation} from '../../models/reservation';
 
 @Component({
   selector: 'app-reservation-list-item',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservation-list-item.component.scss']
 })
 export class ReservationListItemComponent implements OnInit {
+  @Input() reservation: Reservation;
 
   constructor() { }
 
   ngOnInit(): void {
+    // ダミーデータを入れる
+    this.reservation = {
+      actualEnd: 0,
+      actualStart: 0,
+      end: 0,
+      id: '',
+      meetingUrl: '',
+      price: 0,
+      start: 0,
+      studentId: ''
+    };
   }
 
 }
