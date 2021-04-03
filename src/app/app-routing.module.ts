f11c4f1e3ff6e1235b124a62daa2754ef723e484
kajimotoh <k.xdlalbx.p@gmail.com>
2021-04-03 11:23:45 +0900

    Edit routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginPageComponent} from "./pages/auth/login-page/login-page.component";
-import {SignupPageComponent} from "./pages/auth/signup-page/signup-page.component";
-import {PasswordResetPageComponent} from "./pages/auth/password-reset-page/password-reset-page.component";
+import {LoginPageComponent} from './pages/auth/login-page/login-page.component';
+import {SignupPageComponent} from './pages/auth/signup-page/signup-page.component';
+import {PasswordResetPageComponent} from './pages/auth/password-reset-page/password-reset-page.component';
+import {RegisterMemberPageComponent} from './pages/register-member-page/register-member-page.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
   {path: 'signup', component: SignupPageComponent},
   {path: 'reset-password', component: PasswordResetPageComponent},
+  {path: 'register-member', component: RegisterMemberPageComponent},
 ];
 
 @NgModule({
